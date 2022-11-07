38578be80f285ea76ee24d52cac0885bcc02f92c
Quazi_Shams_Kabir <quaziraj12@gmail.com>
2022-11-07 10:02:06 +0530

    primitives added

---
+++ Java/primitives.java
@@ -0,0 +1,10 @@
+public class primitives {
+    public static void main(String[] args) {
+        int a = 8;
+        float b = 8.5f;
+        double c = 8.841844154;  //for long float
+        long d = 8257471415454L; //for long int
+        char e = 's';
+        boolean f = true;        //true/false
+    }
+}
