ec85669eaa00e825f1a4fee0668b4708f37f977c
Quazi_Shams_Kabir <quaziraj12@gmail.com>
2022-11-08 10:39:12 +0530

    primitives modified

--- Java/primitives.java
+++ Java/primitives.java
@@ -1,10 +1,12 @@
 public class primitives {
     public static void main(String[] args) {
-        int a = 8;
-        float b = 8.5f;
-        double c = 8.841844154;  //for long float
-        long d = 8257471415454L; //for long int
-        char e = 's';
+        byte g = 120;  //range -128 to 127 ,8bit
+        short h = 32767; // range -32,768 to 32,767 , 16 bit
+        int a = 8;      // 32bit
+        float b = 8.5f;  //32 bit with 1 bit for sign 8 bit for expo and 23 bit for mantissa
+        double c = 8.841844154;  //for long float , 64 bit with 1 bit for sign 11 bit for expo and 52 bit for mantissa
+        long d = 8257471415454L; //for long int , 64 bit
+        char e = 's';     //16 bit unicode
         boolean f = true;        //true/false
     }
 }
