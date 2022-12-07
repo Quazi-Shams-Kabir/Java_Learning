73e630d6b3c678affd33fe82ab25b55f9c8143cf
Quazi_Shams_Kabir <quaziraj12@gmail.com>
2022-12-07 11:46:35 +0530

    Area Of Isosceles Triangle added

---
+++ Java/Condition-loops/assignments/AreaofIsoscelesTriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AreaofIsoscelesTriangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+                System.out.println("Enter the length of base: ");
+                int b = in.nextInt();
+                System.out.println("Enter the length of hypotenuse: ");
+                int a = in.nextInt();
+                float h = (float) Math.sqrt((a*a)-((b*b)/4.0));
+                System.out.println("Height is: "+h);
+                System.out.println("Area of Isosceles triangle is " +(b*h)/2);
+                System.out.println("Enter 0 to exit");
+                int loop=in.nextInt();
+                if (loop==0) break;
+        }
+    }
+}
+//Math.sqrt((4*(a*a))-(b*b))*b)/4
