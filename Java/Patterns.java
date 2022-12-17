e028c97b9b2758bb400f2ad18e5b9b0d200c6b55
Quazi_Shams_Kabir <quaziraj12@gmail.com>
2022-12-17 22:39:49 +0530

    Pattern10 and 11 added

--- Java/Patterns.java
+++ Java/Patterns.java
@@ -1,7 +1,7 @@
 public class Patterns {
     public static void main(String[] args) {
         int n=5;
-        pattern9(n);
+        pattern11(n);
     }
     static void pattern1(int n){
         for (int i = 0; i < n ; i++) {
@@ -90,6 +90,28 @@ public class Patterns {
             }
             for (int j = 1; j <=i+i-1; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern10(int n){
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 0; j <= n-i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k <i ; k++) {
+                System.out.print(" *  ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern11(int n){
+        for (int i = 0; i < n ; i++) {
+            for (int k = 0; k <i ; k++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(" *  ");
             }
             System.out.println();
         }
