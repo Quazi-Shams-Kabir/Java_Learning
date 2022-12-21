8af608b13c296fc5d8ab55377b5da7897ca61b3e
Quazi_Shams_Kabir <quaziraj12@gmail.com>
2022-12-21 12:33:07 +0530

    Pattern14 added

--- Java/Patterns.java
+++ Java/Patterns.java
@@ -1,7 +1,7 @@
 public class Patterns {
     public static void main(String[] args) {
         int n=5;
-        pattern13(n);
+        pattern14(n);
     }
     static void pattern1(int n){
         for (int i = 0; i < n ; i++) {
@@ -139,6 +139,22 @@ public class Patterns {
     }
     static void pattern13(int n) {
         for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (i == n || j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    static void pattern14(int n) {
+        for (int i = n; i > 0; i--) {
             for (int j = i; j < n; j++) {
                 System.out.print(" ");
             }
