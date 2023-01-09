3ae85941fd50cf131bb863322feda0449fcdae87
Quazi_Shams_Kabir <quaziraj12@gmail.com>
2023-01-09 11:51:42 +0530

    Fibo in recursion added

---
+++ Java/Recursion/Fibo.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class Fibo {
+    public static void main(String[] args) {
+        //index starting from 0
+        System.out.println(fibo(3));
+    }
+    static int fibo(int n){
+        if (n<2){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+}
